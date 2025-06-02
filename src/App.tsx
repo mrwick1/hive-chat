16c2106884265381fe50a8955f809eef1b4403c9
Arjun KR <arjunkrishnaraj123@gmail.com>
2025-06-02 20:42:17 +0530

    chore: initialize react project with vite and typescript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl text-white">HiveChat</h1>
+    </div>
+  );
+}
+
+export default App;
